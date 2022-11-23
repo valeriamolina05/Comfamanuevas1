5796ce9154bef4c66612858fa8cb7ccb3030c82c
cesdeleanmuag <115880909+cesdeleanmuag@users.noreply.github.com>
2022-11-23 11:31:59 -0500

    se crean ejemplos de variables y condiciones

---
+++ recibiendodatos.py
@@ -0,0 +1,16 @@
+#entradas del programa
+numero1=10
+numero2=20
+
+#entradas por teclado
+numero3=int(input("Digita un numero positivo: "))
+numero4=int(input("Digita un numero positivo: "))
+
+#procesos
+#1.proceso aritmetico
+suma=numero1+numero2
+suma2=numero3+numero4
+
+#salidas del programa 
+print(suma)
+print(suma2)
